04a8b1896fd3264a02cd70aeb382ae291dab9862
Kyle Rudy <kdrudy@kyles-mbp.apollosrc.root>
2016-12-09 15:05:03 -0600

    Updated Spark port and jar file

--- src/com/theironyard/Main.java
+++ src/com/theironyard/Main.java
@@ -6,13 +6,25 @@ public class Main {
 
     public static void main(String[] args) {
 
+        Spark.port(getHerokuAssignedPort());
+
         Spark.staticFileLocation("public");
 
         Spark.init();
 
         Spark.get("/api/hello", (
                 (request, response) -> {
-            return "Hello World"; })
+                    return "Hello World";
+                })
         );
+    }
+
+
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
     }
 }
